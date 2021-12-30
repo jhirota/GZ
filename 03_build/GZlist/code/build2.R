862914db5413a2e1f8101dbabe1fb5eda88e8935
jhirota <jhirota@g.ecc.u-tokyo.ac.jp>
2021-12-30 18:40:06 +0900

    Updates on gitignore

--- 03_build/GZlist/code/build2.R
+++ 03_build/GZlist/code/build2.R
@@ -4,7 +4,118 @@ library(reshape2)
 library(lubridate)
 
 
-# GZall_list by week -------
+cities <- c("甲府市", "富士吉田市", "都留市","山梨市", "大月市", "韮崎市", "南アルプス市", "北杜市",
+            "甲斐市", "笛吹市", "上野原市", "甲州市", "中央市", "市川三郷町", "早川町", "身延町",
+            "南部町", "富士川町", "昭和町", "道志村", "西桂町", "忍野村", "山中湖村", "鳴沢村",
+            "富士河口湖町", "小菅村", "丹波山村")
+
+# data load------
+yamanashi <- jpn_pref(19) #yamanashi mapping data
+
+# data clean -------
+GZlist <-read.csv("02_bring/GZlist/data/GZlist.csv") %>% 
+  filter(Keido <= 139.5) %>% 
+  filter(Ido >= 35 & Ido <= 37) %>% 
+  filter(Type == "Food")
+
+for (i in 1:nrow(GZlist)){
+  for(j in 1:length(cities)){
+    if(GZlist[i, 5] %in% grep(cities[j], GZlist[i, 5], value = TRUE)){
+      GZlist[i, "city"] <- cities[j]
+    }else{
+    } 
+  }
+}
+
+# only once
+# write.csv(GZlist, "03_build/GZlist/output/GZlist_Food.csv")
+
+table <- table(GZlist$city)
+data <- data.frame(table)
+names(data)[1] <- "city"
+
+yamanashi$city <- gsub("西八代郡 ", "", yamanashi$city)
+yamanashi$city <- gsub("南都留郡 ", "", yamanashi$city)
+yamanashi$city <- gsub("北都留郡 ", "", yamanashi$city)
+yamanashi$city <- gsub("中巨摩郡 ", "", yamanashi$city)
+yamanashi$city <- gsub("南巨摩郡 ", "", yamanashi$city)
+
+yamanashi <- left_join(yamanashi, data, by = "city") 
+
+# Frequency
+Freq100 <- subset(yamanashi, subset = (Freq >= 100))["city"][[1]]
+Freq200 <- subset(yamanashi, subset = (Freq >= 200))["city"][[1]]
+
+write.csv(Freq100, "03_build/GZlist/output/Freq100.csv", row.names = FALSE)
+
+
+# data wrangling GZ40週時点(2020/10/04まで) ---------
+vresasdata <- read.csv("02_bring/Vresas/data/weekly_vresas.csv")
+GZlist$Date_approval <- as.Date(GZlist$Date_approval)
+
+GZlist40th <- GZlist %>% 
+  filter(Date_approval <= 18539) # 2020/10/04まで
+
+table2 <- table(GZlist40th$city) %>% 
+  data.frame()
+names(table2)[1] <- "city"
+names(table2)[2] <- "Freq40th"
+
+yamanashi <- left_join(yamanashi, table2, by = "city") 
+
+
+# data wrangling GZ52週時点(2020/12/27まで) ----------
+GZlist52nd <- GZlist %>% 
+  filter(Date_approval <= 18623)
+
+table3 <- table(GZlist52nd$city) %>% 
+  data.frame()
+names(table3)[1] <- "city"
+names(table3)[2] <- "Freq52nd"
+
+yamanashi <- left_join(yamanashi, table3, by = "city") 
+
+# yamanashi_comp <- yamanashi
+# only once
+# write.csv(yamanashi_comp, "03_build/GZlist/output/GZ_mapping.csv")
+
+
+# GZ list with time series making ---
+
+N <- nrow(GZlist)
+createEmptyDf = function( nrow, ncol, colnames = c() ){
+  data.frame( matrix( vector(), nrow, ncol, dimnames = list( c(), colnames ) ) )
+}
+x = createEmptyDf(N, 27, colnames = cities )
+
+t_GZlist <- cbind(GZlist, x)
+
+
+for (i in 1:nrow(t_GZlist)){
+  for(j in 1:length(cities)){
+    if(t_GZlist[i, 10] %in% cities[j]){
+      t_GZlist[i, 11:37] <- 0
+      t_GZlist[i, cities[j]] <- 1
+    }else{
+    } 
+  }
+} 
+
+
+t_GZlist <- t_GZlist %>% 
+  melt(id.vars = "Date_approval",
+       measure.vars = cities,
+       variable.name = "city",
+       value.name = "Nofcases") %>% 
+  group_by(Date_approval, city) %>% 
+  summarize(sum = sum(Nofcases)) %>% 
+  ungroup()
+
+
+# write.csv(t_GZlist, "03_build/GZlist/output/GZlist_timeseries.csv")
+
+
+# GZall times series------------------
 
 #Not only food but hotels, wineries etc.
 GZall_list <-read.csv("02_bring/GZlist/data/GZlist.csv") %>% 
@@ -20,46 +131,34 @@ for (i in 1:nrow(GZall_list)){
   }
 }
 
-GZall_list2 <- GZall_list %>% 
-  select(Date_approval, Type) %>% 
-  mutate(Approved = 1)
+N = nrow(GZall_list)
+createEmptyDf = function( nrow, ncol, colnames = c() ){
+  data.frame( matrix( vector(), nrow, ncol, dimnames = list( c(), colnames ) ) )
+}
+x = createEmptyDf( N, 27, colnames = cities )
+t_GZall_list <- cbind(GZall_list, x)
 
 
-GZall_list2$Date_approval <- as.Date(GZall_list2$Date_approval)
-GZall_list2 <- complete(GZall_list2,
-                        Date_approval = seq.Date(min(Date_approval), max(Date_approval), by="day"),
-                        fill = list(Approved = 0))
-GZall_list2 <- complete(GZall_list2, Date_approval, Type, fill = list(Approved = 0, Type = "Food"))
+for (i in 1:nrow(t_GZall_list)){
+  for(j in 1:length(cities)){
+    if(t_GZall_list[i, 10] %in% cities[j]){
+      t_GZall_list[i, 11:37] <- 0
+      t_GZall_list[i, cities[j]] <- 1
+    }else{
+    } 
+  }
+} 
 
-GZall_list2$week <- floor_date(GZall_list2$Date_approval, "week",
-                               week_start = getOption("lubridate.week.start", 1))
-
-
-GZall_list3 <- GZall_list2 %>% 
-  group_by(week, Type) %>% 
-  summarize(GZnew = sum(Approved)) %>% 
-  ungroup() %>% 
-  complete(week, Type, fill = list(GZnew = 0))
-
-
-GZall_list3$cumGZ <- ave(GZall_list3$GZnew, GZall_list3$Type, FUN = cumsum)
-
-write.csv(GZall_list3, "03_build/GZlist/output/GZalllist_week.csv", row.names=FALSE)
+t_GZall_list <- t_GZall_list %>% 
+  melt(id.vars = "Date_approval",
+       measure.vars = cities,
+       variable.name = "city",
+       value.name = "Nofcases") %>% 
+  group_by(Date_approval, city) %>% 
+  summarize(sum = sum(Nofcases)) %>% 
+  ungroup()
 
 
-# GZall_list_wide by week -------
+write.csv(t_GZall_list, "03_build/GZlist/output/GZalllist_timeseries.csv", row.names=FALSE)
 
-GZall_list4 <- data.frame(week = levels(factor(GZall_list3$week)),
-                          cumGZFood = GZall_list3$cumGZ[GZall_list3$Type == "Food"],
-                          GZnewFood = GZall_list3$GZnew[GZall_list3$Type == "Food"],
-                          cumGZHotel = GZall_list3$cumGZ[GZall_list3$Type == "Hotel"],
-                          GZnewHotel = GZall_list3$GZnew[GZall_list3$Type == "Hotel"],
-                          cumGZShuzou = GZall_list3$cumGZ[GZall_list3$Type == "Shuzou"],
-                          GZnewShuzou = GZall_list3$GZnew[GZall_list3$Type == "Shuzou"],
-                          cumGZWinery = GZall_list3$cumGZ[GZall_list3$Type == "Winery"],
-                          GZnewWinery = GZall_list3$GZnew[GZall_list3$Type == "Winery"],
-                          cumGZTransition = GZall_list3$cumGZ[GZall_list3$Type == "Transition"],
-                          GZnewTransition = GZall_list3$GZnew[GZall_list3$Type == "Transition"])
-GZall_list4$week <- as.Date(GZall_list4$week)
 
-write.csv(GZall_list4, "03_build/GZlist/output/GZalllist_week_wide.csv", row.names=FALSE)
--- 04_analyze/GZlist_cum/code/analyze.R
+++ 04_analyze/GZlist_cum/code/analyze.R
@@ -1,6 +1,40 @@
-library(tidyverse)
 
-# Appendix1 A.2 GZ累計のプロット from plot.R --------
+# GZ all plot -------
+GZall_list3 <- read.csv("03_build/GZlist/output/GZalllist_week.csv")
+
+png("04_analyze/GZlist_cum/output/FoodandHotel.png")
+plot(y = GZall_list3$GZnew[GZall_list3$Type == "Food"], x = GZall_list3$GZnew[GZall_list3$Type == "Hotel"])
+dev.off()
+
+png("04_analyze/GZlist_cum/output/FoodandHotel2.png")
+plot(y = GZall_list3$cumGZ[GZall_list3$Type == "Food"],
+     x = GZall_list3$cumGZ[GZall_list3$Type == "Hotel"],
+     xlim = c(0, 4500),
+     ylim = c(0, 4500))+
+  abline(1, 1)
+dev.off()
+
+a <- ggplot(data = GZall_list3,
+       mapping = aes(x = week, y = cumGZ, color = Type)) +
+  geom_point(size = 0.5) +
+  geom_line()
+# ggsave("04_analyze/GZlist_cum/output/GZall_cum.png", a, width = 6, height = 4, dpi = 300)
+
+b <- ggplot(data = GZall_list3,
+            mapping = aes(x = week, y = GZnew, color = Type)) +
+  geom_point(size = 0.5) +
+  geom_line()
+# ggsave("04_analyze/GZlist_cum/output/GZall_new.png", b, width = 6, height = 4, dpi = 300)
+
+png("04_analyze/GZlist_cum/output/cumGZ_week.png")
+plot(y = GZall_list3$cumGZ,
+     x = GZall_list3$week,
+     color = GZall_list3$Type)
+dev.off()
+
+
+
+# GZ累計のプロット --------
 GZcumplot <- read.csv("03_build/weekSIR/output/weekSIR2.csv") %>% 
   filter(pref == "Yamanashi") %>% 
   filter(weeknum >= 12)
@@ -41,4 +75,5 @@ gz_week_caseplot
 
 
 ggsave("04_analyze/GZlist_cum/output/GZ_week_plot.png", gz_week_caseplot, width = 10, height = 8, dpi = 300)
+
 
